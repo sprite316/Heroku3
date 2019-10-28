03e5b750d0c240bc481d193991f9cea5f410f20f
sprite316 <star854@nate.com>
2019-10-29 00:06:16 +0900

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,10 +81,22 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangoyoon3',
+        'USER': 'sprite316',
+        'PASSWORD': 'lambo777',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
