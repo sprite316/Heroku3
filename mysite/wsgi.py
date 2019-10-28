a7f4e7556ff60f416bd79c56206064e8862c8f84
sprite316 <star854@nate.com>
2019-10-29 00:20:15 +0900

    test

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -12,8 +12,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-#application = get_wsgi_application()
-
-
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+application = get_wsgi_application()
