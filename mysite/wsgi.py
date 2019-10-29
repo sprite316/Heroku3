e8cb7bab42be83b177ddba6ce7a5230550420610
sprite316 <star854@nate.com>
2019-10-29 22:42:26 +0900

    test

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,18 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-#import os
-#from django.core.wsgi import get_wsgi_application
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-#application = get_wsgi_application()
+import os
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+application = get_wsgi_application()
 
 
 ###아래꺼 하고나서 안됨
-import os
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+#import os
+#from django.core.wsgi import get_wsgi_application
+#from whitenoise.django import DjangoWhiteNoise
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+#application = get_wsgi_application()
+#application = DjangoWhiteNoise(application)
