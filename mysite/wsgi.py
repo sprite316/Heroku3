2955a25d29d20a020adb1570c25a4738a20bee36
sprite316 <star854@nate.com>
2019-10-29 22:59:02 +0900

    test

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -16,7 +16,7 @@ application = get_wsgi_application()
 ###아래꺼 하고나서 안됨
 #import os
 #from django.core.wsgi import get_wsgi_application
-#from whitenoise.django import DjangoWhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 #application = get_wsgi_application()
 #application = DjangoWhiteNoise(application)
