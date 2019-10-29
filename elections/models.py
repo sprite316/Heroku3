8290f13db4fce38fce2eb78dac49068ad3f9b59b
sprite316 <star854@nate.com>
2019-10-29 23:50:51 +0900

    test

--- elections/models.py
+++ elections/models.py
@@ -7,7 +7,7 @@ class Candidate(models.Model):
     introduction = models.TextField()
     area = models.CharField(max_length=15)
     #party = models.CharField(null=True, max_length=15)
-    party_number = models.TextField()
+    party_number = models.TextField(default='', unique=True)
 
     def __str__(self):
         return self.name
