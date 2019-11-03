e71874d197d637f928bb1fb03aba6f1e70cf6025
sprite316 <star854@nate.com>
2019-11-03 12:16:46 +0900

    test

--- elections/models.py
+++ elections/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Candidate(models.Model):
-    name = models.TextField(null=True)
+    name = models.CharField(max_length=10)
     introduction = models.TextField()
-    area = models.CharField(max_length=15)
+    area = models.TextField(null=True)
     party_number = models.TextField(null=True)
 
     def __str__(self):
