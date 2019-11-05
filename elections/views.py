b3c315edc43a023cd67354716bcdd77ce093f986
sprite316 <star854@nate.com>
2019-11-05 21:52:53 +0900

    test

--- elections/views.py
+++ elections/views.py
@@ -1,33 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import Candidate
-
 import json
-# Create your views here.
-
+from django.core.paginator import Paginator
+'''
 def index(request):
-    #Candidate.objects.all().delete()
     candidates = Candidate.objects.all()
     context = {'candidates':candidates}
     return render(request, 'elections/index.html', context)
-
 '''
+
 def index(request):
-    Candidate.objects.all().delete()
     candidates = Candidate.objects.all()
 
-    with open('./elections/title_link.json', 'rt', encoding='utf-8-sig') as json_file:
-        json_data = json.load(json_file)
+    candidate_list = Candidate.objects.all()
+    paginator = Paginator(candidate_list, 20)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
 
-    for i in range(len(json_data)):
-    #for i in range(5):
-        new_candidate = Candidate(date=json_data[i]["day"],
-        title=json_data[i]["title"],
-        count=json_data[i]["count"],
-        link=json_data[i]["link"])
-        new_candidate.save()
-
-    context = {'candidates':candidates}
-    return render(request, 'elections/index.html', context)
-'''
+    return render(request, 'elections/index.html', {
+    'candidates':candidates, 'posts':posts
+    })
