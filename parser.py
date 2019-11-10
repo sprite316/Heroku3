73353fedd91c4226852a859830dbaf1778589b97
sprite316 <star854@nate.com>
2019-11-10 11:14:44 +0900

    test

--- parser.py
+++ parser.py
@@ -7,7 +7,8 @@ django.setup()
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as BS
 import json
-
+import schedule
+import time
 from elections.models import Candidate
 Candidate.objects.all().delete()
 
@@ -61,17 +62,75 @@ def ygosu_parsing():
     #toJson(temp_list)
     return temp_list
 
-if __name__=='__main__':
-    parsed_data = []
-    parsed_data = ygosu_parsing()
-    #parsed_data1 = ou_parsing()
-    #parsed_data.extend(parsed_data1)
-    toJson(parsed_data)
+def ou_parsing():
+    temp_dict = {}
+    temp_list = []
 
-    for i in range(len(parsed_data)):
-        new_candidate = Candidate(date=parsed_data[i]["day"],
-        title=parsed_data[i]["title"],
-        count=parsed_data[i]["count"],
-        link=parsed_data[i]["link"])
-        #image=parsed_data[i]["image"])
-        new_candidate.save()
+    for page in range(1,8):
+        fullurl = 'http://www.todayhumor.co.kr/board/list.php?table=humorbest&page={}'.format(page)
+        url = urllib.request.Request(fullurl, headers={'user-agent' : 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36', 'Accept-Charset': 'utf-8'})
+        html = urlopen(url)
+        source = html.read()
+        html.close()
+        soup = BS(source, "html.parser")
+        #print(soup)
+        table = soup.find(class_="table_list")
+        tits = table.find_all(class_="subject")
+        counts = table.find_all(class_="hits")
+        days = table.find_all(class_="date")
+        for tit, count, day in zip(tits, counts, days):
+            title = tit.a.get_text()
+            link = 'http://www.todayhumor.co.kr'+tit.a.get('href')
+            url = urllib.request.Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+            '''
+            #image
+            html = urlopen(url)
+            source = html.read()
+            html.close()
+            soup = BS(source, "html.parser")
+            container = soup.find(class_='viewContent')
+            if container.find('video'):
+                videotag = container.find('video')
+                image =videotag.get('poster')
+            elif container.find('img'):
+                imgtag = container.find('img')
+                image =imgtag.get('src')
+            else:
+                image = 'none'
+            #
+            '''
+            read = count.get_text()
+            date = day.get_text()
+            #temp_dict = {'day': date, 'title': title, 'count': read, 'link': link, 'image': image}
+            temp_dict = {'day': date, 'title': title, 'count': read, 'link': link}
+            temp_list.append(temp_dict)
+    #toJson(temp_list)
+    return temp_list
+
+
+def job():
+    if __name__=='__main__':
+        parsed_data = []
+        parsed_data = ygosu_parsing()
+        parsed_data1 = ou_parsing()
+        parsed_data.extend(parsed_data1)
+        toJson(parsed_data)
+
+        for i in range(len(parsed_data)):
+            new_candidate = Candidate(date=parsed_data[i]["day"],
+            title=parsed_data[i]["title"],
+            count=parsed_data[i]["count"],
+            link=parsed_data[i]["link"]
+            #image=parsed_data[i]["image"]
+                                  )
+            new_candidate.save()
+
+schedule.every(20).seconds.do(job)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+
+
+
