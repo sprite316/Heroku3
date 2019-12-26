4398c459b32d331104c6cb8db1936484f52737f7
sprite316 <star854@nate.com>
2019-12-27 00:10:56 +0900

    test

--- parser.py
+++ parser.py
@@ -228,10 +228,9 @@ def clien_parsing():
     for page in range(0, 1):
         url = 'https://www.clien.net/service/group/clien_all?&od=T33&po={}'.format(
             page)
-        #req = requests.get(url, headers=headers)
-        cookies = {'session_id': 'CDNSEC=e19a50f57ff50fc4b8485dd88ef59115'}
-        req = requests.get(url, cookies=cookies)
-        html = req.text
+        #req = requests.get(url)
+        #html = req.text
+        html = urlopen(url)
         soup = BS(html, "html.parser")
         print(soup)
         time.sleep(3)
