e8028dc2daccc05bb891de05199279d219895c6c
sprite316 <star854@nate.com>
2020-02-16 11:21:27 +0900

    test

--- hoobang/admin.py
+++ hoobang/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import hoobang
+#from .models import Candidate, Poll, Choice
 
 # Register your models here.
+
+admin.site.register(hoobang)
+#admin.site.register(Poll)
+#admin.site.register(Choice)
